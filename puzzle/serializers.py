b5cf4a979bd010933ec13dd6450b24d362280f5f
John Hash <johnhash@frozen.com>
2025-02-15 21:53:20 +0100

    feat(Puzzle): create serializer

---
+++ puzzle/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from puzzle.models import Puzzle
+
+
+class PuzzleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Puzzle
+        fields = [
+            "id",
+            "name",
+            "brand",
+            "piece_count",
+            "description",
+            "condition",
+            "image_url",
+            "status",
+            "is_published",
+            "owner",
+        ]
