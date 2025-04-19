6f22401abea004d3769a2eb75d262dd1dfda54f9
John Hash <johnhash@frozen.com>
2025-04-19 17:12:27 +0200

    feat(User): add city in serializer and create route

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from city.models import City
+from city.serializers import CitySerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
+    city = CitySerializer(read_only=True)
     class Meta:
         model = get_user_model()
         fields = [
@@ -12,10 +16,12 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "image",
+            "city"
         ]
 
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer, UserSerializer):
+    city_id = serializers.IntegerField(write_only=True, required=False)
     class Meta:
         model = get_user_model()
         fields = [
@@ -26,6 +32,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             "last_name",
             "password",
             "image",
+            "city",
+            "city_id",
         ]
         extra_kwargs = {"password": {"write_only": True}}
 
@@ -33,9 +41,19 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         username = validated_data["username"]
         email = validated_data["email"]
         password = validated_data["password"]
-        
+        city_id = validated_data["city_id"]
+
+        city = None
+        if city_id:
+            try:
+                city = City.objects.get(id=city_id)
+            except City.DoesNotExist:
+                raise serializers.ValidationError(
+                    {"city_id": "La ville spécifiée n'existe pas."}
+                )
+
         User = get_user_model()
-        new_user = User.objects.create(email=email, username=username)
+        new_user = User.objects.create(email=email, username=username, city=city)
         new_user.set_password(password)
         new_user.save()
         return new_user
