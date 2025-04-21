0266b64a4626ac91cd3b7ba7c047dd1f1a0ba02c
John Hash <johnhash@frozen.com>
2025-04-21 17:46:56 +0200

    feat(Puzzle): add a random route to retrieve 16 random puzzle for the home page

--- puzzle/views.py
+++ puzzle/views.py
@@ -1,5 +1,10 @@
-from rest_framework import permissions, viewsets
+import random
 
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from category.models import Category
 from permissions import IsOwnerOrIsAdminOrReadOnly
 from puzzle.models import Puzzle
 from puzzle.serializers import PuzzleSerializer
@@ -17,3 +22,21 @@ class PuzzleViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=["get"])
+    def random(self, request):
+        categories = list(Category.objects.all())
+        selected_categories = random.sample(categories, 4)
+        result = []
+        for category in selected_categories:
+            puzzles = list(Puzzle.objects.filter(category=category, is_published=True))
+            selected_puzzles = random.sample(puzzles, min(4, len(puzzles)))
+            result.append(
+                {
+                    "puzzles": PuzzleSerializer(selected_puzzles, many=True).data,
+                    "category": category.name,
+                    "category_id": category.id,
+                }
+            )
+
+        return Response(result)
