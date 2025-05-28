87aa07fa6fb4f8332b2b3c3835b810d533cf2866
JohnHash <john@hash.com>
2025-05-29 00:11:28 +0200

    feat(Puzzle): improve query set to filter by category, brand or city

--- apps/puzzle/views.py
+++ apps/puzzle/views.py
@@ -15,6 +15,19 @@ class PuzzleViewSet(viewsets.ModelViewSet):
     serializer_class = PuzzleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.query_params.get("category")
+        brand_id = self.request.query_params.get("brand")
+        city_name = self.request.query_params.get("city")
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        if brand_id:
+            queryset = queryset.filter(brand_id=brand_id)
+        if city_name:
+            queryset = queryset.filter(owner__city__name__iexact=city_name)
+        return queryset
+
     def get_permissions(self):
         if self.action in ["update", "partial_update", "destroy"]:
             return [IsOwnerOrIsAdminOrReadOnly()]
