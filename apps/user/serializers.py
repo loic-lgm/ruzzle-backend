c85778bfe787e3ae021678db408cafdfff83ac2f
JohnHash <john@hash.com>
2025-08-02 22:05:25 +0200

    feat(User): add created_at in serializer

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "image", "city"]
+        fields = ["id", "username", "email", "first_name", "last_name", "image", "city", "created_at"]
 
 
 class UserRegistrationSerializer(UserSerializer):
