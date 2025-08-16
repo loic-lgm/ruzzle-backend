60fbe7927507c02f93846b763a81bfd2429ad0d8
JohnHash <john@hash.com>
2025-08-16 17:36:38 +0200

    feat(Notif): add permissions_classes + authent_classes + admin

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "notif_type",
+        "short_content",
+        "is_read",
+        "created_at",
+    )
+    list_filter = ("notif_type", "is_read", "created_at")
+    search_fields = ("user__username", "content")
+    ordering = ("-created_at",)
+
+    def short_content(self, obj):
+        return obj.content[:50] + ("..." if len(obj.content) > 50 else "")
+
+    short_content.short_description = "Content"
--- apps/notification/views.py
+++ apps/notification/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets, permissions
+
+from apps.utils.authentication import CookieJWTAuthentication
 from .models import Notification
 from .serializers import NotificationSerializer
 
 
-class NotificationViewSet(viewsets.ModelViewSet):
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = NotificationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [CookieJWTAuthentication]
 
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user).order_by(
