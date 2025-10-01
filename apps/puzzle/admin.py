430430562c20d7f6f9231459bfdeb8dc76958c28
JohnHash <john@hash.com>
2025-10-01 17:54:12 +0200

    feat(PuzzleAdmin): add categories

--- apps/puzzle/admin.py
+++ apps/puzzle/admin.py
@@ -17,7 +17,13 @@ class PuzzleAdmin(admin.ModelAdmin):
         "is_published",
         "created",
         "owner",
+        "display_categories",
     )
+
+    def display_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all()])
+
+    display_categories.short_description = "Categories"
 
 
 admin.site.register(Puzzle, PuzzleAdmin)
