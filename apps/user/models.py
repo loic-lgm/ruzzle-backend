2ac41d9a7d030e408916982bb6e27c29ceec7426
JohnHash <john@hash.com>
2025-10-17 18:05:56 +0200

    feat(User): add city related fields to models

--- apps/user/models.py
+++ apps/user/models.py
@@ -30,6 +30,10 @@ class User(AbstractUser):
     email = models.EmailField(max_length=100, unique=True)
     image = models.CharField(max_length=800, blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
+    city_name = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, null=True, blank=True)
     city = models.ForeignKey(
         City, on_delete=models.PROTECT, related_name="users", null=True, blank=True
     )
